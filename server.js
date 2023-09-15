088852dcd5da35fdcacb62f136bca9c217b61e41
oksananaumova <oksana.naumova@mustpay.tech>
2023-09-15 15:50:49 -0400

    added bugs)

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 200) + 1;
+};
+
 app.get("/", function (req, res) {
   res.send(
     '<h1>99 Bottles of beer on the wall</h1> <a href="/98"><h1>Take one down, pass it around</h1></a>'
+  );
+});
+
+app.get("/bugs", function (req, res) {
+  const randomNumber = getRandomNumber();
+
+  res.send(
+    `<h1>99 little bugs in the code</h1> <a href="/bugs/${randomNumber}"><h1>Take one down, patch it around</h1></a>`
   );
 });
 
@@ -16,9 +28,18 @@ app.get("/:number_of_bottles", function (req, res) {
         }"><h1>Take one down, pass it around</h1></a>`
       : `<a href="/"><h1>Start over</h1></a>`;
   res.send(
-    `<h1>${
-      req.params.number_of_bottles
-    } Bottles of beer on the wall.</h1> ${link}`
+    `<h1>${req.params.number_of_bottles} Bottles of beer on the wall.</h1> ${link}`
+  );
+});
+
+app.get("/bugs/:number_of_bugs", function (req, res) {
+  const randomNumber = getRandomNumber();
+  const link =
+    req.params.number_of_bugs > 0
+      ? `<a href="/bugs/${randomNumber}"><h1>Take one down, patch it around</h1></a>`
+      : `<a href="/bugs"><h1>Start over</h1></a>`;
+  res.send(
+    `<h1>${req.params.number_of_bugs} little bugs in the code</h1> ${link}`
   );
 });
 
